60f3e78fcdb8f4686e32132168cdb082af351967
Lucas Guillerot lguiller <lguiller@student.42.fr>
2023-11-23 18:55:31 +0100

    remove forgotten line

--- chrome/content.js
+++ chrome/content.js
@@ -22,7 +22,6 @@ buttonYT.addEventListener("click", () => {
 let buttonTTV = document.createElement("button");
 buttonTTV.classList = "ScCoreButton-sc-ocjdkq-0 caieTg ScButtonIcon-sc-9yap0r-0 dOOPAe";
 buttonTTV.innerHTML = "<svg class=\"ScIconSVG-sc-1q25cff-1 jpczqG\" height=\"100%\" width=\"100%\" viewBox=\"-6 -6 36 36\"><path d=\"M4 3h14l2.707 2.707a1 1 0 0 1 .293.707V20a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1zm3 1v5h9V4H7zm-1 8v7h12v-7H6zm7-7h2v3h-2V5z\"/></svg>";
-buttonTTV.innerHTML = "Save";
 buttonTTV.addEventListener("click", () => {
 	let time = document.querySelector('[data-a-target="player-seekbar-current-time"]').innerHTML;
 	time = time.split(':');
